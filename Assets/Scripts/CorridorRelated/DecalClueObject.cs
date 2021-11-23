5bb1640ff0c1e431ff3e148f106263fa6e0ed486
JohnDoubleUB <up741132@myport.ac.uk>
2021-11-23 14:37:09 +0000

    Created Decal Clue object for handling the display of clues, coded the material for these decals to instance themselves as for some reason that doesn't happen by default?

---
+++ Assets/Scripts/CorridorRelated/DecalClueObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DecalClueObject : MonoBehaviour
+{
+    public string clue;
+    private string lastClue;
+
+    public SpriteDecal[] decals;
+
+    private void Awake()
+    {
+        DisableAllDecals();
+    }
+
+    private void Update()
+    {
+        UpdateDecals();
+    }
+
+    private void UpdateDecals() 
+    {
+        if (clue != lastClue) 
+        {
+            DisableAllDecals();
+            for (int i = 0; i < clue.Length && i < decals.Length; i++) 
+            {
+                SpriteDecal currentDecalObject = decals[i];
+                currentDecalObject.character = clue[i];
+                currentDecalObject.gameObject.SetActive(true);
+            }
+
+            lastClue = clue;
+        }
+    }
+
+    private void DisableAllDecals() 
+    {
+        foreach (GameObject decalObj in decals.Select(x => x.gameObject)) decalObj.SetActive(false);
+    }
+}
--- Assets/Scripts/CorridorRelated/SpriteDecal.cs
+++ Assets/Scripts/CorridorRelated/SpriteDecal.cs
@@ -5,7 +5,7 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class SpriteDecal : MonoBehaviour
 {
-    DecalProjector decalProjector;
+    private DecalProjector decalProjector;
 
     public char character;
 
@@ -15,6 +15,9 @@ public class SpriteDecal : MonoBehaviour
     private void Start()
     {
         decalProjector = GetComponent<DecalProjector>();
+        
+        //Instance the material because for some reason it doesn't do this automatically?
+        decalProjector.material = new Material(decalProjector.material);
     }
 
     private void Update()
