696035a5aec5546cd6db1b1a64cfd771441fa50a
JohnDoubleUB <up741132@myport.ac.uk>
2021-12-20 18:21:26 +0000

    Added in padlock puzzle objects and started work on contoller

---
+++ Assets/Scripts/PuzzleControllers/PadlockController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadlockController : PuzzleElementController
+{
+    public PuzzleElementNotifier PadlockNotifier;
+    public InteractableObject RequiredObject;
+    float pickupSpeedMultiplier = 1.5f;
+
+    private void Awake()
+    {
+        if (PadlockNotifier != null) PadlockNotifier.puzzleToNotify = this;
+    }
+
+
+    public override void Notify(PuzzleElementNotifier notifier = null)
+    {
+        print("lalala");
+    }
+}
