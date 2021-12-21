97d2dcc577244f7770caacab431f98ca8236a34c
JohnDoubleUB <up741132@myport.ac.uk>
2021-12-21 22:17:41 +0000

    Finished adding basic lock animations

--- Assets/Scripts/PuzzleControllers/PadlockController.cs
+++ Assets/Scripts/PuzzleControllers/PadlockController.cs
@@ -13,7 +13,7 @@ public class PadlockController : PuzzleElementController
     public Transform lockFocus;
     public Transform lockHole;
 
-    public float lockRotationUnlockOffset = 30f;
+    public Animator lockAnimator;
 
     private Vector3 keyParentDefaultPos;
     private Quaternion keyParentDefaultRot;
@@ -63,6 +63,7 @@ public class PadlockController : PuzzleElementController
             PickupableInteractable item = inventoryItem.RemoveItemFromContents(false);
             item.transform.position = Vector3.zero;
             InsertKey(item);
+            PuzzleSolved = true;
 
         }
         else 
@@ -83,10 +84,11 @@ public class PadlockController : PuzzleElementController
 
         while (positionValue < 1)
         {
+            await Task.Yield();
             positionValue += Time.deltaTime * pickupSpeedMultiplier;
             smoothedPositionValue = Mathf.SmoothStep(0, 1, positionValue);
             lockFocus.rotation = Quaternion.RotateTowards(initialRotation, targetRotation, smoothedPositionValue * 360f);
-            await Task.Yield();
+            
         }
         
         lockFocus.rotation = targetRotation;
@@ -122,6 +124,26 @@ public class PadlockController : PuzzleElementController
             smoothedPositionValue = Mathf.SmoothStep(0, 1, positionValue);
             keyItem.transform.localRotation = Quaternion.Euler(Vector3.Lerp(initialRotation, targetVector, smoothedPositionValue));
             keyItem.transform.localPosition = Vector3.Lerp(initialPosition, targetVector, smoothedPositionValue);
+            await Task.Yield();
+        }
+
+
+        if (lockAnimator != null) lockAnimator.Play("Unlock");
+        await Task.Delay(1000);
+
+        RotateLock(false);
+
+        positionValue = 0;
+
+        Vector3 lockFocusPos = lockFocus.localPosition;
+        Vector3 lockFocusPosTarget = lockFocus.localPosition - new Vector3(0, 5, 0);
+
+
+        while (positionValue < 1)
+        {
+            positionValue += Time.deltaTime * 0.5f;
+            smoothedPositionValue = Mathf.SmoothStep(0, 1, positionValue);
+            lockFocus.localPosition = Vector3.Lerp(lockFocusPos, lockFocusPosTarget, smoothedPositionValue);
             await Task.Yield();
         }
 
