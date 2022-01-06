dd38cc6e206bdf22955983358f3107170778a34e
JohnDoubleUB <up741132@myport.ac.uk>
2022-01-06 17:30:17 +0000

    Notes can now be zoomed into and moved up and down if needed, some work still required to get working quite right

--- Assets/Scripts/Interactables/InteractableNote.cs
+++ Assets/Scripts/Interactables/InteractableNote.cs
@@ -9,6 +9,7 @@ public class InteractableNote : InteractableObject
     private Material NoteMaterial;
     private bool NoteInitiated;
     private bool inTransitionState;
+    private bool NoteBeingHeld;
 
     private Quaternion defaultRotation;
     private Vector3 defaultPosition;
@@ -20,8 +21,19 @@ public class InteractableNote : InteractableObject
     public Texture NoteTexture;
     public Mesh[] PossibleMeshes;
 
+    public bool allowViewZoom;
+    public float zoomLimit = 0.2f;
+    public float verticalMoveLimit = 5;
+
     public float pickupSpeedMultiplier = 2.5f;
     public float distanceToStopFromPlayer = 0.3f;
+
+    private Vector3 viewRestingPosition;
+    private Vector3 maximumZoomPosition;
+    private Vector3 maximumUpPosition;
+
+    private float currentScrollValue;
+    private float currentVerticalValue;
 
     private void Awake()
     {
@@ -49,6 +61,25 @@ public class InteractableNote : InteractableObject
         }
     }
 
+    private void Update()
+    {
+        //View limit stuff;
+        if (allowViewZoom && NoteBeingHeld && !inTransitionState)
+        {
+            float mouseScrollValue = (Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime) * 30;
+            float mouseMoveYValue = Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0f) currentScrollValue = Mathf.Clamp(currentScrollValue + mouseScrollValue, 0, 1);
+            if (Input.GetAxis("Mouse Y") != 0f) currentVerticalValue = Mathf.Clamp(currentVerticalValue + mouseMoveYValue, -1, 1);
+
+            transform.position = 
+                viewRestingPosition + (transform.forward * (zoomLimit * currentScrollValue))
+                + (transform.up * (verticalMoveLimit * (currentVerticalValue * currentScrollValue)));
+
+
+        }
+    }
+
     protected override void OnInteract()
     {
         ShowToPlayer();
@@ -64,15 +95,16 @@ public class InteractableNote : InteractableObject
         float positionValue = 0;
         float smoothedPositionValue;
         Vector3 cameraPosition = Vector3.zero;
+        Vector3 cameraLookPosition = Vector3.zero;
         Quaternion lookRotation = Quaternion.identity;
 
         while (positionValue < 1f) 
         {
-
+            cameraLookPosition = GameManager.current.playerController.playerCamera.transform.position;
             cameraPosition = GameManager.current.playerController.playerCamera.transform.position + (GameManager.current.playerController.playerCamera.transform.forward * distanceToStopFromPlayer);
             positionValue += Time.deltaTime * pickupSpeedMultiplier;
             smoothedPositionValue = Mathf.SmoothStep(0, 1, positionValue);
-            lookRotation = Quaternion.LookRotation(cameraPosition - transform.position);
+            lookRotation = Quaternion.LookRotation(cameraLookPosition - transform.position);
 
 
             transform.SetPositionAndRotation(
@@ -83,12 +115,22 @@ public class InteractableNote : InteractableObject
             await Task.Yield();
         }
 
+
         transform.SetPositionAndRotation(
               cameraPosition,
               lookRotation
               );
 
+        maximumZoomPosition = cameraLookPosition + (GameManager.current.playerController.playerCamera.transform.forward * zoomLimit); ;
+        maximumUpPosition = transform.position + (transform.up * verticalMoveLimit);
+        viewRestingPosition = cameraPosition;
+
+        currentVerticalValue = 0;
+        currentScrollValue = 0;
         inTransitionState = false;
+        NoteBeingHeld = true;
+
+        //Store current position so we can use it to do some interactive-y things
     }
 
     public void PutDownItem() 
@@ -99,6 +141,7 @@ public class InteractableNote : InteractableObject
     private async void ReturnToDefaultPosition() 
     {
         inTransitionState = true;
+        NoteBeingHeld = false;
         Vector3 positionAtTimeOfPutDown = transform.localPosition;
         Quaternion rotationAtTimeOfPutDown = transform.localRotation;
         float positionValue = 0;
