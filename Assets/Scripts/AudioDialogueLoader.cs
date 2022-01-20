376a1152caaa7a2241b0d47a224890d7ce430cee
JohnDoubleUB <up741132@myport.ac.uk>
2022-01-20 15:54:48 +0000

    Added dialogue audio, and subtitles files not read yet

---
+++ Assets/Scripts/AudioDialogueLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDialogueLoader : MonoBehaviour
+{
+    public AudioClip[] JohnCasterClips;
+    public AudioClip[] TheEntityClips;
+
+    public TextAsset CasterSubtitles;
+    public TextAsset TheEntitySubtitles;
+
+    public string[][] JohnCasterSubtitlesConverted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(CasterSubtitles.text);
+        JohnCasterSubtitlesConverted = JsonUtility.FromJson<string[][]>(TheEntitySubtitles.text);
+        //CasterSubtitles.
+
+        print(JohnCasterSubtitlesConverted.Length);
+
+        foreach (string[] s in JohnCasterSubtitlesConverted) 
+        {
+            print(s[0]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
