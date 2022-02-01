5fc2193495100599f962f0b4c27eae7fb802e074
JohnDoubleUB <up741132@myport.ac.uk>
2022-02-01 17:10:04 +0000

    Added sound for when the mouse walks

--- Assets/Scripts/MouseBob.cs
+++ Assets/Scripts/MouseBob.cs
@@ -9,6 +9,9 @@ public class MouseBob : MonoBehaviour
     public MouseEntity mouseEntity;
     public bool allowMouseBob = true;
 
+    public AudioSource mouseFootstepSource;
+    public AudioClip[] mouseWalkingSounds;
+
     float timer = 0;
     float defaultPosY = 0;
 
@@ -21,17 +24,23 @@ public class MouseBob : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (allowMouseBob && mouseEntity.MovementAmount > 0.01f)
+        if (allowMouseBob && mouseEntity.MovementAmount > 0.001f)
         {
             //Player is moving
             timer += Time.deltaTime * walkingBobbingSpeed;
             transform.localPosition = new Vector3(transform.localPosition.x, defaultPosY + Mathf.Sin(timer) * bobbingAmount, transform.localPosition.z);
+            if (!mouseFootstepSource.isPlaying) 
+            {
+                mouseFootstepSource.clip = mouseWalkingSounds[Random.Range(0, mouseWalkingSounds.Length)];
+                mouseFootstepSource.Play();
+            }
         }
         else
         {
             //Idle
             timer = 0;
             transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Lerp(transform.localPosition.y, defaultPosY, Time.deltaTime * walkingBobbingSpeed), transform.localPosition.z);
+            if (mouseFootstepSource.isPlaying) mouseFootstepSource.Stop();
         }
     }
 }
