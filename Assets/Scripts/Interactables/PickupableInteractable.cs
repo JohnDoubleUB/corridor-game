f2fda6387968cc18a806168453387fb6ad87da25
JohnDoubleUB <up741132@myport.ac.uk>
2022-03-17 13:09:11 +0000

    All pickupables now have pickup id's for easier identifying of them when saving and loading

--- Assets/Scripts/Interactables/PickupableInteractable.cs
+++ Assets/Scripts/Interactables/PickupableInteractable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PickupableInteractable : InteractableObject
 {
+    public int PickupID;
     public PickupType pickupType;
     public InventorySlot currentSlot;
     public float pickupSpeedMultiplier = 2.5f;
