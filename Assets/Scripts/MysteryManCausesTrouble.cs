feea56b9c420c79ca876eecac2e58fa28043c7c5
JohnDoubleUB <up741132@myport.ac.uk>
2022-04-20 17:46:30 +0100

    Possibly fixed script

--- Assets/Scripts/MysteryManCausesTrouble.cs
+++ Assets/Scripts/MysteryManCausesTrouble.cs
@@ -20,42 +20,45 @@ public class MysteryManCausesTrouble : MonoBehaviour, ICustomCorridorEvent
                 if (layoutAndIndex != null)
                 {
                     //Get the plinth controller data
-                    PlinthControllerData puzzleData = layoutAndIndex.layout.puzzleData[0] as PlinthControllerData;
-                    //Get a random placed item if there are any
-                    var placedPuzzlePieceToRemove = puzzleData.PlinthItemsPlaced.Select((isPlaced, index) => new { isPlaced, index }).Shuffle().FirstOrDefault(x => x.isPlaced);
-
-                    if (placedPuzzlePieceToRemove != null) 
+                    if (layoutAndIndex.layout.puzzleData.Any())
                     {
-                        puzzleData.PlinthItemsPlaced[placedPuzzlePieceToRemove.index] = false; //Change this item to not be placed
-                        layoutAndIndex.layout.puzzleData[0] = puzzleData; //Set this back into the layout
-                        layoutAndIndex.layout.spawnableItems[0] = true; //Set note to appear
-                        CorridorChangeManager.current.ReplaceLevelLayoutData(levelAndIndex.index, layoutAndIndex.index, layoutAndIndex.layout); //Replace this data
+                        PlinthControllerData puzzleData = layoutAndIndex.layout.puzzleData[0] as PlinthControllerData;
+                        //Get a random placed item if there are any
+                        var placedPuzzlePieceToRemove = puzzleData.PlinthItemsPlaced.Select((isPlaced, index) => new { isPlaced, index }).Shuffle().FirstOrDefault(x => x.isPlaced);
 
-                        //Find the plinth controller
-                        PlinthController plinthController = CorridorChangeManager.current.Levels[levelAndIndex.index].CorridorLayouts[layoutAndIndex.index].PuzzleElements[0] as PlinthController;
-                        
-                        if (plinthController != null) 
+                        if (placedPuzzlePieceToRemove != null)
                         {
-                            //Find the name of the object we just removed from the puzzle
-                            string pieceName = plinthController.Plinths[placedPuzzlePieceToRemove.index].RequiredObject.ObjectName;
+                            puzzleData.PlinthItemsPlaced[placedPuzzlePieceToRemove.index] = false; //Change this item to not be placed
+                            layoutAndIndex.layout.puzzleData[0] = puzzleData; //Set this back into the layout
+                            layoutAndIndex.layout.spawnableItems[0] = true; //Set note to appear
+                            CorridorChangeManager.current.ReplaceLevelLayoutData(levelAndIndex.index, layoutAndIndex.index, layoutAndIndex.layout); //Replace this data
 
-                            //Find the layout data for the layout we are putting it in
-                            var layoutAndIndex2 = levelAndIndex.level.CorridorLayoutData.Select((layout, index) => new { layout, index }).FirstOrDefault(x => x.layout.LayoutID == "13_2");
+                            //Find the plinth controller
+                            PlinthController plinthController = CorridorChangeManager.current.Levels[levelAndIndex.index].CorridorLayouts[layoutAndIndex.index].PuzzleElements[0] as PlinthController;
 
-                            //Find the LayoutHandler so we can find the index of the collected item
-                            CorridorLayoutHandler trueLayout = CorridorChangeManager.current.Levels[levelAndIndex.index].CorridorLayouts[layoutAndIndex2.index];
-                            var pieceWithIndex = trueLayout.Pickups.Select((item, index) => new { item, index }).FirstOrDefault(x => x.item.PickupItemPrefab.ObjectName == pieceName);
+                            if (plinthController != null)
+                            {
+                                //Find the name of the object we just removed from the puzzle
+                                string pieceName = plinthController.Plinths[placedPuzzlePieceToRemove.index].RequiredObject.ObjectName;
 
-                            layoutAndIndex2.layout.collectedItems.Remove(pieceWithIndex.index);
+                                //Find the layout data for the layout we are putting it in
+                                var layoutAndIndex2 = levelAndIndex.level.CorridorLayoutData.Select((layout, index) => new { layout, index }).FirstOrDefault(x => x.layout.LayoutID == "13_2");
 
-                            CorridorChangeManager.current.ReplaceLevelLayoutData(levelAndIndex.index, layoutAndIndex2.index, layoutAndIndex2.layout); //Replace this data
+                                //Find the LayoutHandler so we can find the index of the collected item
+                                CorridorLayoutHandler trueLayout = CorridorChangeManager.current.Levels[levelAndIndex.index].CorridorLayouts[layoutAndIndex2.index];
+                                var pieceWithIndex = trueLayout.Pickups.Select((item, index) => new { item, index }).FirstOrDefault(x => x.item.PickupItemPrefab.ObjectName == pieceName);
+
+                                layoutAndIndex2.layout.collectedItems.Remove(pieceWithIndex.index);
+
+                                CorridorChangeManager.current.ReplaceLevelLayoutData(levelAndIndex.index, layoutAndIndex2.index, layoutAndIndex2.layout); //Replace this data
+                            }
+
+                            //var pickupToPutElsewhere = GameManager.current.PickupablesIndex.Pickupables.Select((item, index) => new { item, index}).FirstOrDefault(x=>x.item.ObjectName == layoutAndIndex.layout.)
+
+                            //Set another peice to hold this piece
+                            //var layoutAndIndex2 = levelAndIndex.level.CorridorLayoutData.Select((layout, index) => new { layout, index }).FirstOrDefault(x => x.layout.LayoutID == "13_2");
+                            //if()
                         }
-                        
-                        //var pickupToPutElsewhere = GameManager.current.PickupablesIndex.Pickupables.Select((item, index) => new { item, index}).FirstOrDefault(x=>x.item.ObjectName == layoutAndIndex.layout.)
-
-                        //Set another peice to hold this piece
-                        //var layoutAndIndex2 = levelAndIndex.level.CorridorLayoutData.Select((layout, index) => new { layout, index }).FirstOrDefault(x => x.layout.LayoutID == "13_2");
-                        //if()
                     }
                 }
             }
