1ce351821e948903a185cff1713b969717bcaa23
JohnDoubleUB <up741132@myport.ac.uk>
2022-04-25 15:46:59 +0100

    Made some tweaks to the death animation

--- Assets/Scripts/HelperScripts/CameraAnimatorFunctionCaller.cs
+++ Assets/Scripts/HelperScripts/CameraAnimatorFunctionCaller.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class CameraAnimatorFunctionCaller : MonoBehaviour
 {
+    public SpriteRenderer playerSprite;
+
+    private void Start()
+    {
+        if(playerSprite != null && !playerSprite.enabled) playerSprite.enabled = true;
+    }
+    public void TogglePlayerSprite() 
+    {
+        if (playerSprite != null) playerSprite.enabled = !playerSprite.enabled;
+    }
+
     public void CallRevertToSave() 
     {
+        if(playerSprite != null) playerSprite.enabled = true;
         if (GameManager.current != null) GameManager.current.RestartCurrentScene();
     }
 }
