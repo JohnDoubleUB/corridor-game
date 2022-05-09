f7fc69361fb0de9d59d8ea1594d873ba43c26d8b
JohnDoubleUB <up741132@myport.ac.uk>
2022-05-09 16:15:53 +0100

    started implementation of Facepunch.Steamworks

---
+++ Assets/Scripts/SteamIntegration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamIntegration : MonoBehaviour
+{
+    void Awake()
+    {
+
+        if (!Steamworks.SteamClient.IsValid)
+        {
+            print("steamworks not intiallized");
+            try
+            {
+                Steamworks.SteamClient.Init(1981590);
+
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+
+            }
+        }
+        else
+        {
+            print("steamworks initialized");
+        }
+
+        if (Steamworks.SteamClient.IsValid) OnBeingValid();
+
+    }
+
+    private void OnBeingValid()
+    {
+        PrintYourName();
+        Steamworks.SteamFriends.OnGameOverlayActivated += OnOverlayActivated;
+
+    }
+
+    private void PrintYourName()
+    {
+        Debug.Log(Steamworks.SteamClient.Name);
+    }
+
+    private void Update()
+    {
+        Steamworks.SteamClient.RunCallbacks();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SteamShutdownProcedure();
+    }
+
+    private void OnOverlayActivated(bool activated) 
+    {
+        if (activated && !GameManager.current.IsPaused) GameManager.current.TogglePauseGame();
+    }
+
+    private void OnDisable()
+    {
+        if (Application.isEditor) SteamShutdownProcedure();
+    }
+
+    private void SteamShutdownProcedure() 
+    {
+        Steamworks.SteamFriends.OnGameOverlayActivated -= OnOverlayActivated;
+        Steamworks.SteamClient.Shutdown();
+    }
+}
