d16cba1a88af77f37723a1479f404d24ed9406eb
JohnDoubleUB <up741132@myport.ac.uk>
2022-05-10 12:36:09 +0100

    Started adding achievement data into the game

---
+++ Assets/Scripts/ScriptableObjects/CG_AchievementData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "AchievementData", menuName = "ScriptableObjects/AchievementDataObject", order = 1)]
+public class CG_AchievementData : ScriptableObject
+{
+    public string Identifier;
+    public string NiceName;
+    public Sprite UnlockedImage;
+    public Sprite LockedImage;
+    [TextArea(6, 10)]
+    public string Description;
+}
