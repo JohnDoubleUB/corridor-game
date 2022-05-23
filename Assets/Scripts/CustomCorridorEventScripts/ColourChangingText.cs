8669368bbebcd83768c6744a81c664c3e43cc6a2
JohnDoubleUB <up741132@myport.ac.uk>
2022-05-23 11:51:23 +0100

    Finish adding credit rooms

---
+++ Assets/Scripts/CustomCorridorEventScripts/ColourChangingText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourChangingText : MonoBehaviour
+{
+    public Image ImageToChangeColour;
+    private float timeLeft;
+    private Color targetColor;
+    private void Update()
+    {
+        if (timeLeft <= Time.deltaTime)
+        {
+            // transition complete
+            // assign the target color
+            ImageToChangeColour.color = targetColor;
+
+            // start a new transition
+            targetColor = new Color(Random.value, Random.value, Random.value);
+            timeLeft = 1.0f;
+        }
+        else
+        {
+            // transition in progress
+            // calculate interpolated color
+            ImageToChangeColour.color = Color.Lerp(ImageToChangeColour.color, targetColor, Time.deltaTime / timeLeft);
+
+            // update the timer
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
