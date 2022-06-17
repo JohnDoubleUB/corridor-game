55681461775a36d2d2c6ad4ab003d72afc53d715
JohnDoubleUB <up741132@myport.ac.uk>
2022-06-17 17:33:33 +0100

    Changes: - Attempt to fix audio for post game credits (not working still) - Updated in game numbers to be more clear - Added options for Vsync and target framrate

---
+++ Assets/Scripts/Testtt/TestApplicationStuff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestApplicationStuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Target Framerate: " + Application.targetFrameRate);
+        print("Vsync Count: " + QualitySettings.vSyncCount);
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
+
+        print("Target Framerate: " + Application.targetFrameRate);
+        print("Vsync Count: " + QualitySettings.vSyncCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
